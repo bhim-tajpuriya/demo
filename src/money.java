7b10a3587b70b6310f0216f5a8cf913e866ca4b3
bhim-tajpuriya <bhimtaj073@gmail.com>
2018-11-29 14:27:42 +0545

    change in money

--- src/money.java
+++ src/money.java
@@ -15,19 +15,19 @@ public class money {
         }if(amount>=100){
             int hundredcount= amount/100;
             amount= amount-hundredcount*100;
-            System.out.println("HundredCount");
+            System.out.println("HundredCount="+hundredcount);
         }if(amount>=50){
             int fiftycount = amount/50;
             amount= amount-fiftycount*50;
-            System.out.println("FiftyCount" +fiftycount);
+            System.out.println("FiftyCount=" +fiftycount);
         }if(amount>=20){
             int twentycount = amount/20;
             amount= amount-twentycount*20;
-            System.out.println("TwentyCount" +twentycount);
+            System.out.println("TwentyCount=" +twentycount);
         }if(amount>=10){
             int tencount = amount/10;
             amount= amount-tencount*10;
-            System.out.println("TenCount" +tencount);
+            System.out.println("TenCount=" +tencount);
         }if(amount>=5){
             int fivecount = amount/5;
             amount= fivecount-amount*5;
@@ -35,11 +35,11 @@ public class money {
         }if(amount>=2){
             int twocount = amount/2;
             amount= twocount-amount*2;
-            System.out.println("TwoCount" +twocount);
+            System.out.println("TwoCount=" +twocount);
         }if(amount>=1){
             int onecount = amount/1;
             amount= amount-onecount*1;
-            System.out.println("OneCount" +onecount);
+            System.out.println("OneCount=" +onecount);
         }
     }
 }
